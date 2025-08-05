821b93dc6fc0843d56fa717b80aef9b56a9f5d23
Niyuta <cheparity@gmail.com>
2025-08-06 01:13:37 +0800

    feat: publish.py to support `assets`; doc: update debug notes in readme; fix(style): *maybe* the problem of baseURL in config

--- readme.md
+++ readme.md
@@ -1,4 +1,16 @@
-# Publish Scripts
-```
+# README
+
+## Obsidian notes publishing scripts
+
+```bash
 uv run --no-project --with python-frontmatter .\scripts\publish.py --content ./content --vault D:\Workspace\ObsidianRelay\MindHub
+```
+
+## Debugging notes
+
+If met style error, you should check `config/_default/hugo.toml`:
+
+```toml
+#! Switching to "/" when debugging locally
+baseURL = "https://cheparity.github.io/"
 ```
--- scripts/publish.py
+++ scripts/publish.py
@@ -1,10 +1,40 @@
 import argparse
+import re
 import shutil
 from pathlib import Path
 
 import frontmatter
 
-# 获取命令行参数
+image_paths = set()
+
+
+def fix_paths(content: str):
+    # rule: ![](assets/xxx.jpg) -> ![](/assets/xxx.jpg)
+    content = re.sub(
+        r"!\[.*?\]\((\s*assets/[^)]+)\)",
+        lambda m: image_paths.add(m.group(1).strip()) or f"![](/{m.group(1).strip()})",
+        content,
+    )
+
+    # rule: <img src="assets/xxx.jpg"> -> <img src="/assets/xxx.jpg">
+    content = re.sub(
+        r'<img([^>]+?)src="\s*(assets/[^"]+)"',
+        lambda m: image_paths.add(m.group(2).strip())
+        or f'<img{m.group(1)}src="/{m.group(2).strip()}"',
+        content,
+    )
+
+    # rule: ![[assets/xxx.jpg]] -> ![](/assets/xxx.jpg)
+    content = re.sub(
+        r"!\[\[\s*assets/([^\]]+?)\s*\]\]",
+        lambda m: image_paths.add(f"assets/{m.group(1).strip()}")
+        or f"![](/assets/{m.group(1).strip()})",
+        content,
+    )
+
+    return content, image_paths
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--content",
@@ -19,53 +49,60 @@ parser.add_argument(
     required=True,
 )
 
-# 我还有把about等博客页面也用Obsidian管理的需求，放在Obsidian中的_page目录中。不需要的话删除跟page相关的代码即可
 parser.add_argument(
     "--page",
     type=str,
     default="pages",
     help="Page folder name (source) to determine which folder in Obsidian Vault should be compied as content/page.",
 )
+
+parser.add_argument(
+    "--assets",
+    type=str,
+    default="assets",
+    help="Assets folder name (source) to store images, audio, etc. Will be directly moved to hugo's `static/assets`",
+)
+
 args = parser.parse_args()
 
 vault = Path(args.vault)
 content = Path(args.content)
 page = Path(args.page)
+assets = Path(args.assets)
 
-post_dest = content / "posts"
-page_dest = content / "page"
 
-#! 危险操作：删除博客仓库中的原博客目录。然后将笔记目录中post字段为true的笔记，复制到博客仓库
-# 这样设计的原因：如果有一部分博客想撤销发布，那不必手动修改博客目录，只需在笔记中把post字段删了，就会从博客目录中消失
-shutil.rmtree(post_dest, ignore_errors=True)
-shutil.rmtree(page_dest, ignore_errors=True)
+#! [DANGEROUS] Remove everything in original hugo/content folder
+shutil.rmtree(content / "posts", ignore_errors=True)
+shutil.rmtree(content / "page", ignore_errors=True)
 
-# Copy files from Obsidian vaults to content folder
-post_dest.mkdir(parents=True, exist_ok=True)
-page_dest.mkdir(parents=True, exist_ok=True)
+#! Copy files from Obsidian vaults to content folder
+(content / "posts").mkdir(parents=True, exist_ok=True)
+(content / "page").mkdir(parents=True, exist_ok=True)
 
-# 复制我的page页（不需要可以删了）
+# copy profile pages
 for item in (vault / page).iterdir():
-    dst = page_dest / item.name
+    dst = content / "page" / item.name
     if item.is_dir():
         shutil.copytree(item, dst, dirs_exist_ok=True)
     else:
         shutil.copy2(item, dst)
 
-# 开始复制笔记
-for root in Path(vault).rglob("*.md"):
-    if ".obsidian" in root.parts or page in root.parts:
+# copy notes
+for item in Path(vault).rglob("*.md"):
+    if ".obsidian" in item.parts or page in item.parts:
         continue
-    article = frontmatter.load(str(root))
+    article = frontmatter.load(str(item))
     if "post" not in article or not article["post"]:
         continue
 
-    dst = post_dest / root.relative_to(vault)
+    dst = (content / "page") / item.relative_to(vault)
     dst.parent.mkdir(parents=True, exist_ok=True)
-    shutil.copy2(root, dst)
+    shutil.copy2(item, dst)
     article = frontmatter.load(dst)
-    del article[
-        "post"
-    ]  # 在博客仓库下，为了美观，删除笔记的post字段并保存（不删其实也可以）
+    del article["post"]
+    article.content = fix_paths(article.content)
     article["draft"] = False
     frontmatter.dump(article, dst)
+
+# copy images in Obsidian's assets/ to /static/assets
+shutil.copytree(vault / assets, "static")
